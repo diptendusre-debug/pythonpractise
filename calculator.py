87c16ad5ad5a1b3d16bbdb504845fe02b1e15c8e
diptendusre <diptendusre@gmail.com>
2026-03-08 11:41:45 +0530

    calculator function

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1=float(input('Enter First Number: '))
+num2=float(input('Enter second number:'))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("The sum is "+str(addition))
+print("The difference is "+str(subtraction))
+print("The product is :"+str(multiplication))
+print("The quotient is :"+str(division))
