80c34477d91c1c89f461cd1a015b10604c536f02
diptendusre <diptendusre@gmail.com>
2026-03-08 11:44:41 +0530

    added type casting and understanding Type

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 num1=float(input('Enter First Number: '))
 num2=float(input('Enter second number:'))
+##Type Casting 
+print ("The first number is "+str(num1))
+
+## Getting the type of num1
+print ("The type is "+str(type(num1)))
 addition=num1+num2
 subtraction=num1-num2
 multiplication=num1*num2
