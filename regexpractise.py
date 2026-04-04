a299899a51508ad7fe49971ce1136403ce842542
diptendusre <diptendusre@gmail.com>
2026-04-04 14:14:44 +0530

    regex practise

--- regexpractise.py
+++ regexpractise.py
@@ -11,3 +11,8 @@ pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
 result = re.findall(pattern, text)
 
 print(result)
+
+newtext="Diptendu Bandyopadhyay"
+pattern1=r"i"
+result1=re.sub(pattern1,"e",newtext)
+print(result1)
