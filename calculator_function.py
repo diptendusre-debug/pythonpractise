dc1ad84f6975972c6f7af8c8e2ff4ec018bc8eab
diptendusre-debug <diptendusre@gmail.com>
2026-04-06 16:22:55 +0000

    Function

--- calculator_function.py
+++ calculator_function.py
@@ -1,9 +1,9 @@
 firstNumber=int(input("Enter the first Number : "))
 secondNumber=int(input("Enter the Second Number : "))
-
+operation =str(input("Enter the operation you want to perfrom i.e Sum,Sub,Mul,Div : "))
 def addition():
     sum= firstNumber+secondNumber
     print("The sum is :"+ str(sum))
 
-
-addition()
+if operation=="Sum":
+    addition()
