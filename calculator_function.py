d57c13ab9cffcca89a4c12395e0b92e6b2c78faf
diptendusre-debug <diptendusre@gmail.com>
2026-04-06 14:25:02 +0000

    First Function

---
+++ calculator_function.py
@@ -0,0 +1,9 @@
+firstNumber=int(input("Enter the first Number : "))
+secondNumber=int(input("Enter the Second Number : "))
+
+def addition():
+    sum= firstNumber+secondNumber
+    print("The sum is :"+ str(sum))
+
+
+addition()
