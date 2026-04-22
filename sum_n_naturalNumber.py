e2e9a06f3c9b1a03442015f08fc86b5b3bf948e0
diptendusre-debug <diptendusre@gmail.com>
2026-04-22 09:04:58 +0000

    commit

--- sum_n_naturalNumber.py
+++ sum_n_naturalNumber.py
@@ -3,3 +3,8 @@ sum=0
 for i in range(1,n+1):
     sum=sum+i
 print("The sum of first ",n," natural number is : ",sum)
+
+while i<n:
+    sum=sum+i
+    i=i+1
+print("The sum of first ",n," natural number is : ",sum)
