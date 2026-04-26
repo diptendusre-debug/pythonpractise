4a52326ee25a498f0811a42df81ddccfb072ae33
diptendusre-debug <diptendusre@gmail.com>
2026-04-26 05:39:44 +0000

    practise2

---
+++ repo/practise2.py
@@ -0,0 +1,29 @@
+#Check if password if strng 
+#password should be more than  8 character 
+#password should contain digits
+#password should contain alpha numeric character
+import re
+password=str(input("Enter the password : "))
+
+def contains_digit(password):
+    for i in  password:
+        if i.isdigit():
+            return True
+    return False
+def contains_alphanum(password):
+    for i in password:
+        if i.isalnum():
+            return True
+    return False
+
+def isPasswordStrong(password):
+    if len(password)>=8:
+        if contains_digit(password):
+            if contains_alphanum(password):
+                print("Password is strong")
+    else:
+        print("Password is not strong")
+
+isPasswordStrong(password)
+        
+
