1401ee5bb4d1c7ade46b6657f3497c5367dbba50
diptendusre-debug <diptendusre@gmail.com>
2026-04-27 07:20:09 +0000

    practise4

---
+++ repo/practise4.py
@@ -0,0 +1,27 @@
+'''
+Question: With a given integral number n, 
+write a program to generate a dictionary that contains (i, i*i) 
+such that is an integral number between 1 and n (both included). and then the program should print the dictionary. 
+Suppose the following input is supplied to the program: 8 Then,
+the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+'''
+#Pseudocode
+'''
+Input number 
+function declare(number)
+create empty dictionary
+run the for loop
+update the dictinoray 
+print the dictionary
+'''
+
+number=int(input("Enter the number :"))
+def createDictionary(number):
+    mydict=dict()
+    newrange=number+1
+    for i in range (newrange):
+        mydict[i]=i**2
+    print(mydict)
+
+createDictionary(number)
+
